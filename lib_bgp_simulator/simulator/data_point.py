f15d72ff986fce7f37487b753869fe4348e36a2b
Justin Furuness <jfuruness@gmail.com>
2021-09-10 04:09:00 +0000

    Changed the data point cls attr

--- lib_bgp_simulator/simulator/data_point.py
+++ lib_bgp_simulator/simulator/data_point.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass
 
-from lib_caida_collector import AS
+from ..engine.bgp_policy import BGPPolicy
 
 @dataclass(frozen=True)
 class DataPoint:
     """Data point in a graph"""
 
     percent_adoption: float
-    ASCls: AS
+    PolicyCls: BGPPolicy
     propagation_round: int
