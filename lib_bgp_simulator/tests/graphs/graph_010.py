f1ba5fca14a3e9bafabb2a3305807f3d427a0ec6
Reynaldo Morillo <reymorillo2@gmail.com>
2021-11-11 17:28:58 -0500

    Fixed the graph of 10. The attacker and victim were in the wrong position.

--- lib_bgp_simulator/tests/graphs/graph_010.py
+++ lib_bgp_simulator/tests/graphs/graph_010.py
@@ -16,8 +16,8 @@ class Graph010(GraphInfo):
             customer_provider_links=set([
                 CPLink(provider_asn=1, customer_asn=3),
                 CPLink(provider_asn=1, customer_asn=2),
-                CPLink(provider_asn=1, customer_asn=ASNs.VICTIM.value),
-                CPLink(provider_asn=3, customer_asn=ASNs.ATTACKER.value),
+                CPLink(provider_asn=1, customer_asn=ASNs.ATTACKER.value),
+                CPLink(provider_asn=3, customer_asn=ASNs.VICTIM.value),
                 CPLink(provider_asn=2, customer_asn=4),
                 CPLink(provider_asn=2, customer_asn=5)
             ])
