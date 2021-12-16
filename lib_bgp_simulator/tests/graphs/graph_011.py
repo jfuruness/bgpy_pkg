0e25e7fb4b99877ecb569413dcdcde8ecc4e703e
Reynaldo Morillo <reymorillo2@gmail.com>
2021-12-15 21:56:59 -0500

    Graph fixes (so graphviz can plot without uparrows for certain graphs 11 and 16)

--- lib_bgp_simulator/tests/graphs/graph_011.py
+++ lib_bgp_simulator/tests/graphs/graph_011.py
@@ -25,9 +25,13 @@ class Graph011(GraphInfo):
                 CPLink(provider_asn=8,customer_asn=3),
                 CPLink(provider_asn=9,customer_asn=1),
                 CPLink(provider_asn=1,customer_asn=5),
+                CPLink(provider_asn=1,customer_asn=15),
+                CPLink(provider_asn=15,customer_asn=5),
                 CPLink(provider_asn=2,customer_asn=11),
                 CPLink(provider_asn=11,customer_asn=12),
                 CPLink(provider_asn=12,customer_asn=ASNs.VICTIM.value),
-                CPLink(provider_asn=3,customer_asn=ASNs.ATTACKER.value)
+                CPLink(provider_asn=14,customer_asn=ASNs.ATTACKER.value),
+                CPLink(provider_asn=3,customer_asn=13),
+                CPLink(provider_asn=13,customer_asn=14),
             ])
     )
