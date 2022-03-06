ccacb86f141d686aa8461cc994f204fdb350cb12
Reynaldo Morillo <reymorillo2@gmail.com>
2022-03-06 15:09:43 -0500

    Fixed the pep8 style issues using a tool a command line tool called black.

--- lib_bgp_simulator/tests/graphs/graph_022.py
+++ lib_bgp_simulator/tests/graphs/graph_022.py
@@ -14,17 +14,17 @@ class Graph022(GraphInfo):
         super(Graph022, self).__init__(
                 peer_links=set([
                     PeerLink(1, 2)
-                    ]),
-            customer_provider_links=set(
-                [CPLink(provider_asn=4, customer_asn=3),
-                 CPLink(provider_asn=4, customer_asn=7),
-                 CPLink(provider_asn=3, customer_asn=1),
-                 CPLink(provider_asn=3, customer_asn=2),
-                 CPLink(provider_asn=1, customer_asn=ASNs.ATTACKER.value),
-                 CPLink(provider_asn=2, customer_asn=5),
-                 CPLink(provider_asn=5, customer_asn=ASNs.VICTIM.value),
-                 CPLink(provider_asn=5, customer_asn=6),
-                 CPLink(provider_asn=6, customer_asn=7),
-                 CPLink(provider_asn=7, customer_asn=8),
-                 CPLink(provider_asn=7, customer_asn=9),
-                 ]))
+                ]),
+                customer_provider_links=set([
+                    CPLink(provider_asn=4, customer_asn=3),
+                    CPLink(provider_asn=4, customer_asn=7),
+                    CPLink(provider_asn=3, customer_asn=1),
+                    CPLink(provider_asn=3, customer_asn=2),
+                    CPLink(provider_asn=1, customer_asn=ASNs.ATTACKER.value),
+                    CPLink(provider_asn=2, customer_asn=5),
+                    CPLink(provider_asn=5, customer_asn=ASNs.VICTIM.value),
+                    CPLink(provider_asn=5, customer_asn=6),
+                    CPLink(provider_asn=6, customer_asn=7),
+                    CPLink(provider_asn=7, customer_asn=8),
+                    CPLink(provider_asn=7, customer_asn=9),
+                ]))
