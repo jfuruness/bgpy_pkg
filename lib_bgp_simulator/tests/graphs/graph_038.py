6de71dafdcbd6e95404a7edb79344f4c5a559be3
Reynaldo Morillo <reymorillo2@gmail.com>
2022-04-04 15:41:35 -0400

    Updated the previous two graphs

--- lib_bgp_simulator/tests/graphs/graph_038.py
+++ lib_bgp_simulator/tests/graphs/graph_038.py
@@ -12,7 +12,15 @@ class Graph038(GraphInfo):
 
     def __init__(self):
         super(Graph038, self).__init__(
-            peer_links=set([PeerLink(135, ASNs.VICTIM.value), PeerLink(34, ASNs.ATTACKER.value), PeerLink(12, 34), PeerLink(11, 33)]),
+            peer_links=set(
+                [
+                    PeerLink(135, ASNs.VICTIM.value),
+                    PeerLink(7, 8),
+                    PeerLink(34, ASNs.ATTACKER.value),
+                    PeerLink(12, 34),
+                    PeerLink(11, 33)
+                ]
+            ),
             customer_provider_links=set(
                 [
                     CPLink(provider_asn=ASNs.VICTIM.value, customer_asn=34),
@@ -21,7 +29,10 @@ class Graph038(GraphInfo):
                     CPLink(provider_asn=12, customer_asn=11),
                     CPLink(provider_asn=34, customer_asn=33),
                     CPLink(provider_asn=11, customer_asn=1),
-                    CPLink(provider_asn=33, customer_asn=1)
+                    CPLink(provider_asn=33, customer_asn=1),
+                    CPLink(provider_asn=1, customer_asn=6),
+                    CPLink(provider_asn=6, customer_asn=7),
+                    CPLink(provider_asn=6, customer_asn=8)
                 ]
             ),
         )
--- lib_bgp_simulator/tests/graphs/graph_039.py
+++ lib_bgp_simulator/tests/graphs/graph_039.py
@@ -12,7 +12,7 @@ class Graph039(GraphInfo):
 
     def __init__(self):
         super(Graph039, self).__init__(
-            peer_links=set([PeerLink(56, 78)]),
+            peer_links=set([PeerLink(56, 78), PeerLink(7, 8)]),
             customer_provider_links=set(
                 [
                     CPLink(provider_asn=56, customer_asn=ASNs.VICTIM.value),
@@ -20,6 +20,9 @@ class Graph039(GraphInfo):
                     CPLink(provider_asn=1, customer_asn=ASNs.VICTIM.value),
                     CPLink(provider_asn=1, customer_asn=3),
                     CPLink(provider_asn=33, customer_asn=3),
+                    CPLink(provider_asn=3, customer_asn=6),
+                    CPLink(provider_asn=6, customer_asn=7),
+                    CPLink(provider_asn=6, customer_asn=8),
                     CPLink(provider_asn=78, customer_asn=ASNs.ATTACKER.value)
                 ]
             ),
