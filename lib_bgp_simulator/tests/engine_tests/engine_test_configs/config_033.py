6657706da3f32c9a3c5a4c8d833c29febb87cba3
Cameron Morris <cameron.morris@uconn.edu>
2022-07-14 00:47:45 +0000

    lint

--- lib_bgp_simulator/tests/engine_tests/engine_test_configs/config_033.py
+++ lib_bgp_simulator/tests/engine_tests/engine_test_configs/config_033.py
@@ -6,7 +6,7 @@ from ..utils import EngineTestConfig
 
 from ....simulation_engine import BGPAS
 from ....simulation_framework import ValidPrefix
-from ....enums import ASNs, Prefixes
+from ....enums import Prefixes
 
 
 class Custom33ValidPrefix(ValidPrefix):
@@ -14,9 +14,11 @@ class Custom33ValidPrefix(ValidPrefix):
 
     __slots__ = ()
 
-    def post_propagation_hook(self, engine=None, propagation_round=0, *args, **kwargs):
+    def post_propagation_hook(self, engine=None, propagation_round=0,
+                              *args, **kwargs):
         if propagation_round == 1:  # second round
-            ann = deepcopy(engine.as_dict[2]._local_rib.get_ann(Prefixes.PREFIX.value))
+            ann = deepcopy(
+                engine.as_dict[2]._local_rib.get_ann(Prefixes.PREFIX.value))
             # Add a new announcement at AS 3, which will be better than the one
             # from 2 and cause a withdrawn route by 1 to 4
             ann.seed_asn = 3
--- lib_bgp_simulator/tests/engine_tests/engine_test_configs/config_034.py
+++ lib_bgp_simulator/tests/engine_tests/engine_test_configs/config_034.py
@@ -6,7 +6,7 @@ from ..utils import EngineTestConfig
 
 from ....simulation_engine import BGPAS
 from ....simulation_framework import ValidPrefix
-from ....enums import ASNs, Prefixes
+from ....enums import Prefixes
 
 
 class Custom34ValidPrefix(ValidPrefix):
@@ -14,9 +14,11 @@ class Custom34ValidPrefix(ValidPrefix):
 
     __slots__ = ()
 
-    def post_propagation_hook(self, engine=None, propagation_round=0, *args, **kwargs):
+    def post_propagation_hook(self, engine=None, propagation_round=0,
+                              *args, **kwargs):
         if propagation_round == 1:  # second round
-            ann = deepcopy(engine.as_dict[2]._local_rib.get_ann(Prefixes.PREFIX.value))
+            ann = deepcopy(
+                engine.as_dict[2]._local_rib.get_ann(Prefixes.PREFIX.value))
             # Add a new announcement at AS 3, which will be better than the one
             # from 2 and cause a withdrawn route by 1 to 4
             ann.seed_asn = 3
@@ -25,18 +27,19 @@ class Custom34ValidPrefix(ValidPrefix):
             Custom34ValidPrefix.victim_asns = {2, 3}
 
         if propagation_round == 2:  # third round
-            ann = deepcopy(engine.as_dict[3]._local_rib.get_ann(Prefixes.PREFIX.value))
+            ann = deepcopy(
+                engine.as_dict[3]._local_rib.get_ann(Prefixes.PREFIX.value))
             ann.withdraw = True
-            # Remove the original announcement from 2
+            # Remove the original announcement from 3
+            # The one from 2 is now the next-best
             engine.as_dict[3]._local_rib.remove_ann(Prefixes.PREFIX.value)
             engine.as_dict[3]._ribs_out.remove_entry(1, Prefixes.PREFIX.value)
             engine.as_dict[3]._send_q.add_ann(1, ann)
             Custom34ValidPrefix.victim_asns = {2}
 
 
-
 class Config034(EngineTestConfig):
-    """Test withdrawal caused by better announcement"""
+    """Test withdrawal choosing the next best announcement"""
 
     name = "034"
     desc = "Test withdrawal mechanism"
