b0f3d5f9a513a5a0bf8a5d2b9b3673b14761e82e
Justin Furuness <jfuruness@gmail.com>
2023-07-09 03:01:18 -0400

    Working v2 graphs

--- bgp_simulator_pkg/simulation_framework/graph_factory.py
+++ bgp_simulator_pkg/simulation_framework/graph_factory.py
@@ -1,10 +1,13 @@
 from collections import defaultdict
+from itertools import product
 from pathlib import Path
 import pickle
 
+import matplotlib  # type: ignore
 import matplotlib.pyplot as plt  # type: ignore
+from tqdm import tqdm
 
-from .metric_tracker import MetricKey
+from .metric_tracker.metric_key import MetricKey
 from .utils import get_all_metric_keys
 
 
@@ -16,32 +19,35 @@ class GraphFactory:
         with self.pickle_path.open("rb") as f:
             self.graph_rows = pickle.load(f)
         self.graph_dir: Path = graph_dir
-        self.graph_dir.mkdirs(parents=True, exist_ok=True)
+        self.graph_dir.mkdir(parents=True, exist_ok=True)
 
     def generate_graphs(self) -> None:
         """Generates default graphs"""
 
         # Each metric key here contains plane, as group, and outcome
         # In other words, aech type of graph
-        for metric_key in get_all_metric_keys():
-            for adopting in [True, False]:
-                relevant_rows = list()
-                for row in self.graph_rows:
-                    # Get all the rows that correspond to that type of graph
-                    BaseASCls = row["data_key"].scenario_config.BaseASCls
-                    AdoptASCls = row["data_key"].scenario_config.AdoptASCls
-                    print("hello")
-                    if (
-                        row["metric_key"].plane == metric_key.plane
-                        and row["metric_key"].as_group == metric_key.as_group
-                        and row["metric_key"].outcome == metric_key.outcome
-                        and (
-                            (row["metric_key"].ASCls == BaseASCls and not adopting)
-                            or (row["metric_key"].ASCls == AdoptASCls and adopting)
-                        )
-                    ):
-                        relevant_rows.append(row)
-                self._generate_graph(metric_key, relevant_rows, adopting=adopting)
+
+        graph_infos = list(product(get_all_metric_keys(), [True, False]))
+
+        for metric_key, adopting in tqdm(
+            graph_infos, total=len(graph_infos), desc="Writing Graphs"
+        ):
+            relevant_rows = list()
+            for row in self.graph_rows:
+                # Get all the rows that correspond to that type of graph
+                BaseASCls = row["data_key"].scenario_config.BaseASCls
+                AdoptASCls = row["data_key"].scenario_config.AdoptASCls
+                if (
+                    row["metric_key"].plane == metric_key.plane
+                    and row["metric_key"].as_group == metric_key.as_group
+                    and row["metric_key"].outcome == metric_key.outcome
+                    and (
+                        (row["metric_key"].ASCls == BaseASCls and not adopting)
+                        or (row["metric_key"].ASCls == AdoptASCls and adopting)
+                    )
+                ):
+                    relevant_rows.append(row)
+            self._generate_graph(metric_key, relevant_rows, adopting=adopting)
 
     def _generate_graph(self, metric_key: MetricKey, relevant_rows, adopting) -> None:
         """Writes a graph to the graph dir"""
@@ -59,16 +65,19 @@ class GraphFactory:
         # Value: float
         # Yerr: yerr
 
+        if not relevant_rows:
+            return
         graph_name = (
             f"{relevant_rows[0]['data_key'].scenario_config.ScenarioCls.__name__}"
             f"_{metric_key.as_group.value}_adopting_is_{adopting}"
-            f"_{metric_key.outcome.value}"
+            f"_{metric_key.outcome.name}"
             f"_{metric_key.plane.value}.png"
         ).replace(" ", "")
         as_cls_rows_dict = defaultdict(list)
         for row in relevant_rows:
-            as_cls_rows_dict[row["scenario_config"].AdoptASCls].append(row)
+            as_cls_rows_dict[row["data_key"].scenario_config.AdoptASCls].append(row)
 
+        matplotlib.use("Agg")
         fig, ax = plt.subplots()
         fig.set_dpi(150)
         # Set X and Y axis size
@@ -81,13 +90,13 @@ class GraphFactory:
                 sorted(graph_rows, key=lambda x: x["data_key"].percent_adopt)
             )
             ax.errorbar(
-                [x["data_key"].percent_adopt for x in graph_rows_sorted],
-                [x["data_key"].value for x in graph_rows_sorted],
-                yerr=[x["data_key"].yerr for x in graph_rows_sorted],
+                [x["data_key"].percent_adopt * 100 for x in graph_rows_sorted],
+                [x["value"] for x in graph_rows_sorted],
+                yerr=[x["yerr"] for x in graph_rows_sorted],
                 label=as_cls.name,
             )
         # Set labels
-        ax.set_ylabel(f"Percent {metric_key.outcome.value}")
+        ax.set_ylabel(f"PERCENT {metric_key.outcome.name}".replace("_", " "))
         ax.set_xlabel("Percent Adoption")
 
         # This is to avoid warnings
