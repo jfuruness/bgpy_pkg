23182f9d9ee7fe0299dd8f3b2a7ca686efa734af
Justin Furuness <jfuruness@gmail.com>
2023-10-28 11:11:56 -0400

    updated engine tester to make it more compatible with others

--- bgpy/tests/engine_tests/utils/engine_tester.py
+++ bgpy/tests/engine_tests/utils/engine_tester.py
@@ -7,7 +7,9 @@ from .diagram import Diagram
 from .engine_test_config import EngineTestConfig
 from .simulator_codec import SimulatorCodec
 from bgpy.simulation_engine import SimulationEngine
-from bgpy.enums import Plane
+from bgpy.simulation_framework import Scenario
+from bgpy.simulation_framework import MetricTracker
+from bgpy.enums import Plane, SpecialPercentAdoptions
 
 
 class EngineTester:
@@ -19,7 +21,38 @@ class EngineTester:
         conf: EngineTestConfig,
         overwrite: bool = False,
         codec: SimulatorCodec = SimulatorCodec(),
+        DiagramCls: type[Diagram] = Diagram,
+        SimulationEngineCls: type[SimulationEngine] = SimulationEngine,
+        compare_metrics: bool = False,
     ):
+        """Regarding the compare_metrics kwarg:
+
+        There was quite a debate on whether or not the engine tester
+        should compare metrics. There are a lot of cons for comparing
+        metrics here. Every single time the metrics change, all of the
+        test data needs to be overwritten. This means even if you add
+        a new metric, the test data needs to be overwritten, even if it
+        is unrelated to the metrics, and you'll need to double check the
+        ground truth of everything once again. metrics are also very
+        finnicky, if JSON or CSV layouts change, they are prone to breakage
+        without actually being a true failure. If the layout changes then
+        those files change in git, even though no code has been modified.
+        On top of that, different
+        simulation_frameworks for different projects have different
+        metrick trackers, some of which are quite different, some of which
+        have no metrics at all. It also breaks
+        the single responsibility of these being engine tests, to test the
+        engine, not the simulation_framework. metric tracker could also be
+        tested with unit tests.
+
+        As for the cons, some tests would require a full round of propagation
+        for a true metric tracker test. While the majority of the feedback
+        was to remove it, one project was already using it. In light of that,
+        being able to compare metrics in this test class will remain a feature,
+        but by default it will be off. I've left an example to show how
+        other projects could implement their own versions of it.
+        """
+
         self.conf = conf
         self.overwrite = overwrite
         self.codec = codec
@@ -29,6 +62,11 @@ class EngineTester:
         # Creates directory for this specific test
         self.test_dir: Path = self.base_dir / self.conf.name  # type: ignore
         self.test_dir.mkdir(exist_ok=True)
+
+        self.DiagramCls: type[Diagram] = DiagramCls
+        self.SimulationEngineCls: type[SimulationEngine] = SimulationEngineCls
+
+        self.compare_metrics: bool = compare_metrics
 
     def test_engine(self):
         """Tests an engine run
@@ -64,15 +102,14 @@ class EngineTester:
         outcomes_yaml = {
             as_obj.asn: result for as_obj, result in data_plane_outcomes.items()
         }
-        # Get stored metrics
-        metric_tracker = self.conf.MetricTrackerCls()
-        metric_tracker.track_trial_metrics(
+        metric_tracker = self._get_trial_metrics(
             engine=engine,
             percent_adopt=0,
             trial=0,
             scenario=scenario,
             propagation_round=self.conf.propagation_rounds - 1,
             outcomes=outcomes,
+
         )
         # Store engine and traceback YAML
         self._store_data(engine, outcomes_yaml, metric_tracker)
@@ -84,7 +121,7 @@ class EngineTester:
     def _get_engine(self, scenario):
         """Creates and engine and sets it up for runs"""
 
-        engine = SimulationEngine(
+        engine = self.SimulationEngineCls(
             BaseASCls=self.conf.scenario_config.BaseASCls,
             peer_links=self.conf.graph.peer_links,  # type: ignore
             cp_links=self.conf.graph.customer_provider_links,  # type: ignore
@@ -92,6 +129,28 @@ class EngineTester:
 
         scenario.setup_engine(engine, scenario)
         return engine
+
+    def _get_trial_metrics(
+        self,
+        engine: SimulationEngine,
+        percent_adopt: float | SpecialPercentAdoptions,
+        trial: int,
+        scenario: Scenario,
+        propagation_round: int,
+        outcomes,
+    ) -> MetricTracker:
+
+        # Get stored metrics
+        metric_tracker = self.conf.MetricTrackerCls()
+        metric_tracker.track_trial_metrics(
+            engine=engine,
+            percent_adopt=0,
+            trial=0,
+            scenario=scenario,
+            propagation_round=self.conf.propagation_rounds - 1,
+            outcomes=outcomes,
+        )
+        return metric_tracker
 
     def _store_data(self, engine, outcomes, metric_tracker):
         """Stores YAML for the engine, outcomes, and CSV for metrics.
@@ -110,6 +169,10 @@ class EngineTester:
         if not self.outcomes_ground_truth_path.exists() or self.overwrite:
             self.codec.dump(outcomes, path=self.outcomes_ground_truth_path)
 
+        if self.compare_metrics:
+            self._store_metrics(metric_tracker)
+
+    def store_metrics(self, metric_tracker: MetricTracker) -> None:
         metric_tracker.write_data(
             csv_path=self.metrics_guess_path_csv,
             pickle_path=self.metrics_guess_path_pickle,
@@ -154,7 +217,7 @@ class EngineTester:
             static_order = False
 
         # Write guess graph
-        Diagram().generate_as_graph(
+        self.DiagramCls().generate_as_graph(
             engine_guess,
             scenario,  # type: ignore
             outcomes_guess,
@@ -166,7 +229,7 @@ class EngineTester:
             view=False,
         )
         # Write ground truth graph
-        Diagram().generate_as_graph(
+        self.DiagramCls().generate_as_graph(
             engine_gt,
             scenario,  # type: ignore
             outcomes_gt,
@@ -190,6 +253,11 @@ class EngineTester:
         outcomes_guess = self.codec.load(self.outcomes_guess_path)
         outcomes_gt = self.codec.load(self.outcomes_ground_truth_path)
         assert outcomes_guess == outcomes_gt
+
+        if self.compare_metrics:
+            self._compare_metrics_to_gt()
+
+    def _compare_metrics_to_gt(self):
         # Compare metrics CSV
         with self.metrics_guess_path_csv.open() as guess_f:
             with self.metrics_ground_truth_path_csv.open() as ground_truth_f:
