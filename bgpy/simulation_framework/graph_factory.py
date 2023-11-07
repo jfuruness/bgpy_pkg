93bfb6b785dc98a41f4c909512ec014e0020e891
Justin Furuness <jfuruness@gmail.com>
2023-11-06 21:00:25 -0500

    Another attempted fix at closing the matplotlib graphs according to their release notes

--- bgpy/simulation_framework/graph_factory.py
+++ bgpy/simulation_framework/graph_factory.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from functools import cached_property
+import gc
 from itertools import product
 from pathlib import Path
 import pickle
@@ -156,6 +157,18 @@ class GraphFactory:
         # there is some sort of a memory leak that occurs. See stackoverflow
         # comment above
         plt.close(fig)
+        # If you are running one simulation after the other, matplotlib
+        # basically leaks memory. I couldn't find the original issue, but
+        # here is a note in one of their releases saying to just call the garbage
+        # collector: https://matplotlib.org/stable/users/prev_whats_new/
+        # whats_new_3.6.0.html#garbage-collection-is-no-longer-run-on-figure-close
+        # and here is the stackoverflow post on this topic:
+        # https://stackoverflow.com/a/33343289/8903959
+        # Even if this works without garbage collection in 3.5.2, that will break
+        # as soon as we upgrade to the latest matplotlib which no longer does
+        # If you run the simulations on a machine with many cores and lots of trials,
+        # this bug leaks enough memory to crash the server, so we must garbage collect
+        gc.collect()
 
     @cached_property
     def markers(self) -> tuple[str, ...]:
