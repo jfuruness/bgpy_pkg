95b4dc52ad6dd3e4acdbaea0dd8226df444a2ce7
Justin Furuness <jfuruness@gmail.com>
2023-12-31 17:23:35 -0500

    fixed traceback bug

--- bgpc/include/cpp_simulation_engine/announcement.hpp
+++ bgpc/include/cpp_simulation_engine/announcement.hpp
@@ -25,40 +25,6 @@ public:
           withdraw(withdraw) {}
 };
 
-/*
-class Announcement {
-public:
-    const std::string prefix;
-    const std::vector<int> as_path;
-    const int timestamp;
-    const std::optional<int> seed_asn;
-    const std::optional<bool> roa_valid_length;
-    const std::optional<int> roa_origin;
-    const Relationships recv_relationship;
-    const bool withdraw;
-    const bool traceback_end;
-    const std::vector<std::string> communities;
-
-    Announcement(const std::string& prefix, const std::vector<int>& as_path, int timestamp,
-                 const std::optional<int>& seed_asn, const std::optional<bool>& roa_valid_length,
-                 const std::optional<int>& roa_origin, Relationships recv_relationship,
-                 bool withdraw = false, bool traceback_end = false,
-                 const std::vector<std::string>& communities = {});
-
-    bool operator==(const Announcement& other) const;
-
-    bool prefix_path_attributes_eq(const Announcement* ann) const;
-    bool invalid_by_roa() const;
-    bool valid_by_roa() const;
-    bool unknown_by_roa() const;
-    bool covered_by_roa() const;
-    bool roa_routed() const;
-    int origin() const;
-};
-*/
-
-
-
 
 class Announcement {
 
--- bgpc/src/cpp_simulation_engine/policies/bgp_simple_policy/process_incoming_funcs.cpp
+++ bgpc/src/cpp_simulation_engine/policies/bgp_simple_policy/process_incoming_funcs.cpp
@@ -194,8 +194,7 @@ std::shared_ptr<Announcement> BGPSimplePolicy::copy_and_process(const std::share
         ann->staticData,
         new_as_path,
         recv_relationship,
-        ann->traceback_end
-        //ann->communities
+        false
     );
 }
 
