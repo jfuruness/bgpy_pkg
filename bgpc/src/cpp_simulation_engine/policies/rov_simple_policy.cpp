acc0f46c50dc511147979b18b8c7f34cb4f12fc4
Justin Furuness <jfuruness@gmail.com>
2024-01-16 22:43:00 -0500

    removing C++ code

--- bgpc/src/cpp_simulation_engine/policies/rov_simple_policy.cpp
+++
@@ -1,6 +0,0 @@
-#include "bgp_simple_policy.hpp"
-#include "rov_simple_policy.hpp"
-
-bool ROVSimplePolicy::valid_ann(const std::shared_ptr<Announcement>& ann, Relationships recv_relationship) const {
-    return !ann->invalid_by_roa() && BGPSimplePolicy::valid_ann(ann, recv_relationship);
-}
