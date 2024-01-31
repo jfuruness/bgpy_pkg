528c80551e7e551b4b84ed38c86ddcda32f69294
Justin Furuness <jfuruness@gmail.com>
2024-01-30 21:58:00 -0500

    optimized recvq

--- bgpy/simulation_engine/ann_containers/recv_queue.py
+++ bgpy/simulation_engine/ann_containers/recv_queue.py
@@ -16,7 +16,16 @@ class RecvQueue(AnnContainer[str, list["Ann"]]):
         We don't use defaultdict here because those are not yamlable
         """
 
-        self.data[ann.prefix] = self.data.get(ann.prefix, list()) + [ann]
+        prefix = ann.prefix
+        dct = self.data
+        list_of_anns = dct.get(prefix)
+        if list_of_anns:
+            list_of_anns.append(ann)
+        else:
+            dct[prefix] = [ann]
+
+        # This implementation is much slower
+        # self.data[ann.prefix] = self.data.get(ann.prefix, list()) + [ann]
 
     def get_ann_list(self, prefix: str) -> list["Ann"]:
         """Returns recevied ann list for a given prefix"""
