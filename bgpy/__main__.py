5054b85f2435bf49fe4eca84069a91a12cffd61b
Justin Furuness <jfuruness@gmail.com>
2024-01-31 02:09:19 -0500

    updated __main__

--- bgpy/__main__.py
+++ bgpy/__main__.py
@@ -20,7 +20,9 @@ def main():
             0.2,
             0.5,
             0.8,
-            SpecialPercentAdoptions.ALL_BUT_ONE,
+            0.99,
+            # Using only 1 AS not adopting causes extreme variance
+            # SpecialPercentAdoptions.ALL_BUT_ONE,
         ),
         scenario_configs=(
             ScenarioConfig(ScenarioCls=SubprefixHijack, AdoptPolicyCls=ROVSimplePolicy),
