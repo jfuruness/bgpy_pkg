b93faaad0cb7e1516a166c9f72fd6c636d9945c3
Justin Furuness <jfuruness@gmail.com>
2024-02-01 02:35:55 -0500

    graph factory class fixes that didn't make it into the original

--- bgpy/simulation_framework/scenarios/preprocess_anns_funcs.py
+++ bgpy/simulation_framework/scenarios/preprocess_anns_funcs.py
@@ -289,12 +289,15 @@ def _find_shortest_non_adopting_path_general(
     # To do this, I'll  simply iterate through all remaining ASes, and then sort
     # them and return the shortest AS path (or None)
     if len(engine.as_graph) > 100:
-        if self_scenario.percent_adoption >= .8:
-            input_clique_ases = (x for x in engine.as_graph if x.input_clique)
+        if self_scenario.percent_adoption >= 0.8:
+            input_clique_ases = [x for x in engine.as_graph if x.input_clique]
             print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" + str(len(input_clique_ases)))
             print(x.policy.name for x in input_clique_ases)
             print(self_scenario.attacker_asns)
-            print(len(engine.as_graph.as_dict[asn].neighbors) for asn in self_scenario.attacker_asns)
+            print(
+                len(engine.as_graph.as_dict[asn].neighbors)
+                for asn in self_scenario.attacker_asns
+            )
         # TODO: fix
         # NOTE: It's just that at 90% adoption, when attackers and victims are
         # on the edge, this code doesn't get run (which is why it's fine)
