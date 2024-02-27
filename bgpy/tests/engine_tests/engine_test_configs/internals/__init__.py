de687ca4e306aa21bbb1885d126a0469060f7ff4
Arvind Kasiliya <arvind@kasiliya.com>
2024-02-27 12:34:33 -0500

    Add test for subprefix hijack with custom announcements and ROAs
    This ensures that ROAs are merged with announcements correctly

--- bgpy/tests/engine_tests/engine_test_configs/internals/__init__.py
+++ bgpy/tests/engine_tests/engine_test_configs/internals/__init__.py
@@ -3,6 +3,7 @@ from .internal_config_001 import internal_config_001
 from .internal_config_002 import internal_config_002
 from .internal_config_003 import internal_config_003
 from .internal_config_004 import internal_config_004
+from .internal_config_005 import internal_config_005
 
 internal_configs = [
     internal_config_000,
@@ -10,6 +11,7 @@ internal_configs = [
     internal_config_002,
     internal_config_003,
     internal_config_004,
+    internal_config_005,
 ]
 
 __all__ = ["internal_configs"]
