397775f5395cf2340e53bb5b5c893cbbd3d3bc40
Justin Furuness <jfuruness@gmail.com>
2024-04-18 04:28:21 -0400

    Fixed ASPA bugs

--- bgpy/simulation_engine/policies/aspa/aspa.py
+++ bgpy/simulation_engine/policies/aspa/aspa.py
@@ -61,42 +61,77 @@ class ASPA(BGP):
             return super()._valid_ann(ann, from_rel)
         else:
             u_min = self._calculate_u_min(ann)  # type: ignore
-            v_max_complement = self._calculate_v_max_complement(ann)
+            v_max = self._calculate_v_max(ann)
 
-            v_max = len(ann.as_path) - v_max_complement
             if u_min <= v_max:
                 return False
             else:
+                # NOTE: everything past step 4 in the RFC does not result
+                # in "invalid", so we omit them
                 return super()._valid_ann(ann, from_rel)
 
     def _calculate_u_min(self, ann: "Ann") -> int:
         """Calculates u_min from ASPA RFC"""
 
         path = ann.as_path
+
+        N = len(path)
+
+        def convert_u_to_index(u: int) -> int:
+            """Converts u to index in array
+
+            For example, take AS path 1, 2, 666, 777
+            In the ASPA RFC, this is  N, 3, 2,   1 indexed
+            And in Python, this is    0, 1, 2,   3 indexed
+            So to go from u to python index, you do:
+            """
+            return N - u
+
         # https://datatracker.ietf.org/doc/html/draft-ietf-sidrops-aspa-verification-16
         u_min = len(path) + 1  # type: ignore
-        reversed_path = path[::-1]
-        # For every adopting ASPA AS in the path,
-        # The next ASN in the path must be in their providers list
-        for i in range(len(reversed_path) - 1):
-            if not self._provider_check(reversed_path[i], reversed_path[i + 1]):
-                u_min = i + 2
+        for u in range(2, N + 1):
+            u_minus_one_index = convert_u_to_index(u - 1)
+            u_index = convert_u_to_index(u)
+            # False from this func is the same as "Not Provider+" from the ASPA RFC
+            if not self._provider_check(path[u_minus_one_index], path[u_index]):
+                u_min = u
                 break
-
         return u_min
 
-    def _calculate_v_max_complement(self, ann: "Ann") -> int:
-        """Calculates v_max_complement from ASPA RFC"""
+    def _calculate_v_max(self, ann: "Ann") -> int:
+        """Calculates v_max from ASPA RFC"""
 
-        v_max_complement = 0
-        for i in range(len(ann.as_path) - 1):
-            if not self._provider_check(ann.as_path[i], ann.as_path[i + 1]):
-                v_max_complement = i + 1
+        N = len(ann.as_path)
+
+        def convert_v_to_index(v: int) -> int:
+            """Converts v to index in array
+
+            For example, take AS path 1, 2, 666, 777
+            In the ASPA RFC, this is  N, 3, 2,   1 indexed
+            And in Python, this is    0, 1, 2,   3 indexed
+            So to go from v to python index, you do:
+            """
+            return N - v
+
+        v_max = 0
+        for v in range(N - 1, 1 - 1, -1):
+            v_index = convert_v_to_index(v)
+            v_plus_one_index = convert_v_to_index(v + 1)
+            # The provider check returning false is the same as the hop func
+            # described in the ASPA RFC section 5 returning "Not Provider+"
+            if not self._provider_check(
+                ann.as_path[v_plus_one_index], ann.as_path[v_index]
+            ):
+                v_max = v
                 break
-        return v_max_complement
+        return v_max
 
     def _provider_check(self, asn1: int, asn2: int) -> bool:
-        """Returns False if asn2 is not in asn1's provider_asns, AND asn1 adopts ASPA"""
+        """Returns False if asn2 is not in asn1's provider_asns, AND asn1 adopts ASPAi
+
+        This also essentially can take the place of the "hop check" listed in
+        ASPA RFC section 5
+        """
 
         cur_as_obj = self.as_.as_graph.as_dict[asn1]
         if isinstance(cur_as_obj.policy, ASPA):
---
+++ bgpy/tests/engine_tests/engine_test_configs/internals/internal_config_004.py
@@ -0,0 +1,67 @@
+from frozendict import frozendict
+
+from bgpy.as_graphs import ASGraphInfo, PeerLink, CustomerProviderLink as CPLink
+from bgpy.enums import ASNs
+from bgpy.simulation_engine import BGP, ASPA
+from bgpy.simulation_framework import (
+    PrefixHijack,
+    ScenarioConfig,
+    preprocess_anns_funcs,
+)
+from bgpy.tests.engine_tests.utils import EngineTestConfig
+
+
+r"""Graph to test ASPA RFC section 12
+
+                    AS 666 - AS(4)
+                      /         \
+      (down-ramp)    /           \    (up-ramp)
+                 AS(2)          AS(3)
+                   /               \
+                  /               AS(777)
+                 /             (Origin AS)
+              AS (1)
+"""
+
+as_graph_info = ASGraphInfo(
+    peer_links=frozenset(
+        [
+            PeerLink(ASNs.ATTACKER.value, 4),
+        ]
+    ),
+    customer_provider_links=frozenset(
+        [
+            CPLink(provider_asn=2, customer_asn=1),
+            CPLink(provider_asn=ASNs.ATTACKER.value, customer_asn=2),
+            CPLink(provider_asn=4, customer_asn=3),
+            CPLink(provider_asn=3, customer_asn=ASNs.VICTIM.value),
+        ]
+    ),
+)
+
+
+internal_config_004 = EngineTestConfig(
+    name="internal_config_004",
+    desc=(
+        "ASPA RFC Section 12. "
+        " Previously this diagram used to show an off by 1 bug for ASPA, so we"
+        " prevent it from popping up again"
+    ),
+    scenario_config=ScenarioConfig(
+        ScenarioCls=PrefixHijack,
+        preprocess_anns_func=preprocess_anns_funcs.origin_hijack,
+        BasePolicyCls=BGP,
+        override_victim_asns=frozenset({ASNs.VICTIM.value}),
+        override_attacker_asns=frozenset({ASNs.ATTACKER.value}),
+        override_non_default_asn_cls_dict=frozendict(
+            {
+                1: ASPA,
+                2: ASPA,
+                3: ASPA,
+                4: ASPA,
+                ASNs.VICTIM.value: ASPA,
+            }
+        ),
+    ),
+    as_graph_info=as_graph_info,
+)
