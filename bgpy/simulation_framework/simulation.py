a258ec8392bce2934b0db7bcf8f81f4cdf0bf027
Justin Furuness <jfuruness@gmail.com>
2024-08-09 00:54:37 -0400

    updated pbar tracking

--- bgpy/simulation_framework/simulation.py
+++ bgpy/simulation_framework/simulation.py
@@ -313,7 +313,7 @@ class Simulation:
         total_completed = 0
         for file_path in self._tqdm_tracking_dir.iterdir():
             total_completed += int(file_path.read_text())
-        pbar.n = total_completed - pbar.n
+        pbar.n = total_completed
         pbar.refresh()
 
     ############################
