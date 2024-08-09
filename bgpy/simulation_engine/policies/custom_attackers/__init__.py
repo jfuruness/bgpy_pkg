e84b54429c112a1e2020418e05f1a895823460d2
Justin Furuness <jfuruness@gmail.com>
2024-08-09 02:32:14 -0400

    changed file names to add prefix

--- bgpy/simulation_engine/policies/custom_attackers/__init__.py
+++ bgpy/simulation_engine/policies/custom_attackers/__init__.py
@@ -1,4 +1,6 @@
-from .first_asn_stripping_aspa_attacker import FirstASNStrippingPrefixASPAAttacker
-from .shortest_path_aspa_attacker import ShortestPathPrefixASPAAttacker
+from .first_asn_stripping_prefix_aspa_attacker import (
+    FirstASNStrippingPrefixASPAAttacker
+)
+from .shortest_path_prefix_aspa_attacker import ShortestPathPrefixASPAAttacker
 
 __all__ = ["FirstASNStrippingPrefixASPAAttacker", "ShortestPathPrefixASPAAttacker"]
