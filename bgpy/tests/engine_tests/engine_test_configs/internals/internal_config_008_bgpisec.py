c913a6628d0a23d9777edb4a72a493a0f670fa38
Triet Huynh <triet403@gmail.com>
2025-03-13 00:53:56 -0400

    amend! first test
    first test

--- bgpy/tests/engine_tests/engine_test_configs/internals/internal_config_008_bgpisec.py
+++ bgpy/tests/engine_tests/engine_test_configs/internals/internal_config_008_bgpisec.py
@@ -14,27 +14,23 @@ from bgpy.tests.engine_tests.utils.engine_test_config import EngineTestConfig
 internal_config_008_bgpisec = EngineTestConfig(
     name="internal_config_008_bgpisec",
     desc="""
-    Taken from the paper, testing BGP-iSec partial path verification, where BGPsec would fail because of non adopting AS (AS 2) would downgrade from that AS onward back to BGP thus allowing the attacker forged announcement to be accepted.
+    Taken from the paper, testing BGP-iSec partial path verification, where BGPsec would fail because of non adopting AS (AS 800) would downgrade from that AS onward back to BGP thus allowing the attacker forged announcement to be accepted.
+    If this scenario is using BGPSec - which from the point of view of AS 9 is just BGP because of downgrade from AS 2 - AS 9 would have picked the attacker AS (because smaller ASN number compare to legitimate AS 800 announcement).
     """,
     scenario_config=ScenarioConfig(
         ScenarioCls=ShortestPathPrefixHijack,
-        BasePolicyCls=BGP,
+        BasePolicyCls=BGPiSecTransitive,
         override_victim_asns=frozenset({ASNs.VICTIM.value}),
         override_attacker_asns=frozenset({ASNs.ATTACKER.value}),
-        hardcoded_asn_cls_dict=frozendict(
-            {
-                1: BGPiSecTransitive,
-                3: BGPiSecTransitive,
-                9: BGPiSecTransitive,
-            }
-        ),
+        propagation_rounds=2,
+        hardcoded_asn_cls_dict=frozendict({800: BGP}),
     ),
     as_graph_info=ASGraphInfo(
         customer_provider_links=frozenset(
             [
-                CPLink(provider_asn=2, customer_asn=ASNs.VICTIM.value),
+                CPLink(provider_asn=800, customer_asn=ASNs.VICTIM.value),
                 CPLink(provider_asn=3, customer_asn=ASNs.VICTIM.value),
-                CPLink(provider_asn=2, customer_asn=9),
+                CPLink(provider_asn=800, customer_asn=9),
                 CPLink(provider_asn=3, customer_asn=ASNs.ATTACKER.value),
                 CPLink(provider_asn=ASNs.ATTACKER.value, customer_asn=9),
             ]
