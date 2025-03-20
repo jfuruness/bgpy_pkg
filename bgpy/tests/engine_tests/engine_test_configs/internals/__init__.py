5f1a490801cafdfde04e86c30a9df3f82e7f975e
Triet Huynh <triet403@gmail.com>
2025-03-20 16:05:29 -0400

    add one more test cases

--- bgpy/tests/engine_tests/engine_test_configs/internals/__init__.py
+++ bgpy/tests/engine_tests/engine_test_configs/internals/__init__.py
@@ -7,6 +7,9 @@ from .internal_config_005 import internal_config_005
 from .internal_config_006 import internal_config_006
 from .internal_config_007 import internal_config_007
 from .internal_config_008_bgpisec import internal_config_008_bgpisec
+from .internal_config_009_bgpisec import internal_config_009_bgpisec
+from .internal_config_010_bgpisec import internal_config_010_bgpisec
+from .internal_config_011_bgpisec import internal_config_011_bgpisec
 
 internal_configs = [
     internal_config_000,
@@ -20,6 +23,7 @@ internal_configs = [
     internal_config_008_bgpisec,
     internal_config_009_bgpisec,
     internal_config_010_bgpisec,
+    internal_config_011_bgpisec,
 ]
 
 __all__ = ["internal_configs"]
